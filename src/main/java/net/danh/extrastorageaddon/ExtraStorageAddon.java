20def16ead4a32c8bbcb1bae0ac00a5f47a33c93
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-04-13 20:41:04 +0700

    Fortune addon

--- src/main/java/net/danh/extrastorageaddon/ExtraStorageAddon.java
+++ src/main/java/net/danh/extrastorageaddon/ExtraStorageAddon.java
@@ -22,6 +22,7 @@ public final class ExtraStorageAddon extends JavaPlugin implements Listener {
             getLogger().log(Level.INFO, "Successfully hooked with ExtraStorage v" + Objects.requireNonNull(getServer().getPluginManager().getPlugin("ExtraStorage")).getDescription().getVersion());
         } else {
             getServer().getPluginManager().disablePlugin(this);
+            return;
         }
         Objects.requireNonNull(getCommand("exaddon")).setExecutor(new Cmd());
         Files.createconfig();
