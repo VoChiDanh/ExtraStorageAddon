136bbe7c38ede7bca25fda333b622857789208c2
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-10-13 01:27:18 +0700

    1.2.3 - Fix explosive

--- src/main/java/net/danh/extrastorageaddon/Manager/ExplosiveBlock.java
+++ src/main/java/net/danh/extrastorageaddon/Manager/ExplosiveBlock.java
@@ -40,37 +40,37 @@ public class ExplosiveBlock {
             for (Location location : locations) {
                 for (ItemStack itemStack : location.getBlock().getDrops()) {
                     if (Files.getconfigfile().getStringList("enchants.explosive.blocks").contains(location.getBlock().getType().name())) {
-                        if (!ExtraStorageAddon.getInstance().getEManager().checkBlock(extraStorageAddon.getEManager().getItem(e.getPlayer(), itemStack).getType())) {
-                            return;
-                        }
-                        try {
-                            if (extraStorageAddon.getEManager().checkFlags(e.getPlayer(), location.getBlock())) {
-                                String fortune = Objects.requireNonNull(Files.getconfigfile().getString("enchants.fortune", "#level# * 5")).replace("#level#", String.valueOf(e.getPlayer().getInventory().getItemInMainHand().getEnchantmentLevel(Enchantment.LOOT_BONUS_BLOCKS)));
-                                fortune = PlaceholderAPI.setPlaceholders(e.getPlayer(), fortune);
-                                if (e.getPlayer().getInventory().getItemInMainHand().containsEnchantment(Enchantment.LOOT_BONUS_BLOCKS)) {
-                                    int amount = (int) Double.parseDouble(Calculator.calculator(fortune, 0));
-                                    /*      if (user.getStorage().getFreeSpace() - (long) (user.getStorage().getMaterial(itemStack) + amount) >= 0) {*/
-                                    if (user.getStorage().isUnused(extraStorageAddon.getEManager().getItem(e.getPlayer(), itemStack))) {
-                                        user.getStorage().addUnused(extraStorageAddon.getEManager().getItem(e.getPlayer(), itemStack), amount);
-                                    } else {
-                                        user.getStorage().addMaterial(extraStorageAddon.getEManager().getItem(e.getPlayer(), itemStack), amount);
-                                    }
-                                    location.getBlock().setType(Material.AIR);
-                                    /*}*/
-                                } else {
-                                    /*if (user.getStorage().getFreeSpace() - (long) (user.getStorage().getMaterial(itemStack) + 1) >= 0) {*/
-                                    if (user.getStorage().isUnused(extraStorageAddon.getEManager().getItem(e.getPlayer(), itemStack))) {
-                                        user.getStorage().addUnused(extraStorageAddon.getEManager().getItem(e.getPlayer(), itemStack), 1);
-                                    } else {
-                                        user.getStorage().addMaterial(extraStorageAddon.getEManager().getItem(e.getPlayer(), itemStack), 1);
+                        if (ExtraStorageAddon.getInstance().getEManager().checkBlock(extraStorageAddon.getEManager().getItem(e.getPlayer(), itemStack).getType())) {
 
+                            try {
+                                if (extraStorageAddon.getEManager().checkFlags(e.getPlayer(), location.getBlock())) {
+                                    String fortune = Objects.requireNonNull(Files.getconfigfile().getString("enchants.fortune", "#level# * 5")).replace("#level#", String.valueOf(e.getPlayer().getInventory().getItemInMainHand().getEnchantmentLevel(Enchantment.LOOT_BONUS_BLOCKS)));
+                                    fortune = PlaceholderAPI.setPlaceholders(e.getPlayer(), fortune);
+                                    if (e.getPlayer().getInventory().getItemInMainHand().containsEnchantment(Enchantment.LOOT_BONUS_BLOCKS)) {
+                                        int amount = (int) Double.parseDouble(Calculator.calculator(fortune, 0));
+                                        /*      if (user.getStorage().getFreeSpace() - (long) (user.getStorage().getMaterial(itemStack) + amount) >= 0) {*/
+                                        if (user.getStorage().isUnused(extraStorageAddon.getEManager().getItem(e.getPlayer(), itemStack))) {
+                                            user.getStorage().addUnused(extraStorageAddon.getEManager().getItem(e.getPlayer(), itemStack), amount);
+                                        } else {
+                                            user.getStorage().addMaterial(extraStorageAddon.getEManager().getItem(e.getPlayer(), itemStack), amount);
+                                        }
+                                        location.getBlock().setType(Material.AIR);
+                                        /*}*/
+                                    } else {
+                                        /*if (user.getStorage().getFreeSpace() - (long) (user.getStorage().getMaterial(itemStack) + 1) >= 0) {*/
+                                        if (user.getStorage().isUnused(extraStorageAddon.getEManager().getItem(e.getPlayer(), itemStack))) {
+                                            user.getStorage().addUnused(extraStorageAddon.getEManager().getItem(e.getPlayer(), itemStack), 1);
+                                        } else {
+                                            user.getStorage().addMaterial(extraStorageAddon.getEManager().getItem(e.getPlayer(), itemStack), 1);
+
+                                        }
+                                        location.getBlock().setType(Material.AIR);
+                                        /*}*/
                                     }
-                                    location.getBlock().setType(Material.AIR);
-                                    /*}*/
                                 }
+                            } catch (NullPointerException ex) {
+                                user.getStorage().addNewMaterial(extraStorageAddon.getEManager().getItem(e.getPlayer(), itemStack));
                             }
-                        } catch (NullPointerException ex) {
-                            user.getStorage().addNewMaterial(extraStorageAddon.getEManager().getItem(e.getPlayer(), itemStack));
                         }
                     }
                 }
